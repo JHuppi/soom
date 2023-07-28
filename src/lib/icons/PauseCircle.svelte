08cc9c5b53542808732b836e968e200e2f7b8e58
Janessa Huppi <jhuppicode@gmail.com>
2023-07-27 20:13:58 -0600

    Prep play and pause icons for animation

---
+++ src/lib/icons/PauseCircle.svelte
@@ -0,0 +1,35 @@
+<script>
+	// https://heroicons.com
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	stroke="currentColor"
+>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+		class="static"
+	/>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+		class="animate"
+	/>
+	<path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9v6m-4.5 0V9" />
+</svg>
+
+<style>
+	.animate {
+		color: transparent;
+	}
+
+	.static {
+		color: var(--color-grey-300);
+	}
+</style>
