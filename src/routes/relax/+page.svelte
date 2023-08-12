5f35df220528a9f05f63ec8d19c65f10fa6a3b89
Janessa Huppi <jhuppicode@gmail.com>
2023-08-12 16:46:58 -0600

    Clean up the 'relax' page

---
+++ src/routes/relax/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Animation from '$lib/Animation.svelte';
+</script>
+
+<h1>relax</h1>
+<Animation />
+
+<style>
+	h1 {
+		font-size: var(--scale-8);
+		margin: 0 auto;
+		text-align: center;
+	}
+</style>
