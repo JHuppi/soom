163004f8f44151840adb0aa8e0249a6c79493665
Janessa Huppi <jhuppicode@gmail.com>
2024-02-13 15:36:35 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m = [
-  e + "/app/immutable/entry/app.944eb0b1.js",
-  e + "/app/immutable/assets/0.418f0db2.css",
-  e + "/app/immutable/nodes/0.8e291915.js",
-  e + "/app/immutable/nodes/1.8c94c711.js",
+  e + "/app/immutable/entry/app.24f91d9f.js",
+  e + "/app/immutable/assets/0.cb58c5e6.css",
+  e + "/app/immutable/nodes/0.49ebd435.js",
+  e + "/app/immutable/nodes/1.da0279ec.js",
   e + "/app/immutable/assets/2.9de49fb8.css",
   e + "/app/immutable/nodes/2.ccd18dc1.js",
   e + "/app/immutable/nodes/3.dfaf519a.js",
@@ -12,15 +12,15 @@ const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m
   e + "/app/immutable/assets/RoutineWrapper.5fca8c1c.css",
   e + "/app/immutable/chunks/RoutineWrapper.03856647.js",
   e + "/app/immutable/chunks/index.83bb740a.js",
-  e + "/app/immutable/chunks/paths.7b53810b.js",
+  e + "/app/immutable/chunks/paths.73537ce3.js",
   e + "/app/immutable/chunks/scheduler.ab84cf82.js",
-  e + "/app/immutable/chunks/singletons.a9b2822b.js",
-  e + "/app/immutable/entry/start.8819c0d0.js"
+  e + "/app/immutable/chunks/singletons.3c3e729b.js",
+  e + "/app/immutable/entry/start.f9278578.js"
 ], l = [
   e + "/favicon.png",
   e + "/manifest.json",
   e + "/soom-logo.svg"
-], u = "1707854239564", o = [...m, ...l], i = `cache-${u}`, p = self;
+], u = "1707863792139", o = [...m, ...l], i = `cache-${u}`, p = self;
 p.addEventListener("install", (a) => {
   const t = async () => {
     await (await caches.open(i)).addAll(o);
