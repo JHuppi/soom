c77dcb1fac1cd1ae6f3087fabd8dac92df2aec5c
Janessa Huppi <jhuppicode@gmail.com>
2024-02-13 12:57:21 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m = [
-  e + "/app/immutable/entry/app.e9fc42d8.js",
+  e + "/app/immutable/entry/app.944eb0b1.js",
   e + "/app/immutable/assets/0.418f0db2.css",
-  e + "/app/immutable/nodes/0.f79fee87.js",
-  e + "/app/immutable/nodes/1.990be2be.js",
+  e + "/app/immutable/nodes/0.8e291915.js",
+  e + "/app/immutable/nodes/1.8c94c711.js",
   e + "/app/immutable/assets/2.9de49fb8.css",
   e + "/app/immutable/nodes/2.ccd18dc1.js",
   e + "/app/immutable/nodes/3.dfaf519a.js",
@@ -12,15 +12,15 @@ const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m
   e + "/app/immutable/assets/RoutineWrapper.5fca8c1c.css",
   e + "/app/immutable/chunks/RoutineWrapper.03856647.js",
   e + "/app/immutable/chunks/index.83bb740a.js",
-  e + "/app/immutable/chunks/paths.4e956f1c.js",
+  e + "/app/immutable/chunks/paths.7b53810b.js",
   e + "/app/immutable/chunks/scheduler.ab84cf82.js",
-  e + "/app/immutable/chunks/singletons.78d4d55a.js",
-  e + "/app/immutable/entry/start.fa8640e1.js"
+  e + "/app/immutable/chunks/singletons.a9b2822b.js",
+  e + "/app/immutable/entry/start.8819c0d0.js"
 ], l = [
   e + "/favicon.png",
   e + "/manifest.json",
   e + "/soom-logo.svg"
-], u = "1707851239744", o = [...m, ...l], i = `cache-${u}`, p = self;
+], u = "1707854239564", o = [...m, ...l], i = `cache-${u}`, p = self;
 p.addEventListener("install", (a) => {
   const t = async () => {
     await (await caches.open(i)).addAll(o);
@@ -38,14 +38,14 @@ p.addEventListener("fetch", (a) => {
   if (a.request.method !== "GET")
     return;
   const t = async () => {
-    const s = new URL(a.request.url), n = await caches.open(i);
+    const s = new URL(a.request.url), c = await caches.open(i);
     if (o.includes(s.pathname))
-      return n.match(s.pathname);
+      return c.match(s.pathname);
     try {
-      const c = await fetch(a.request);
-      return c.status === 200 && n.put(a.request, c.clone()), c;
+      const n = await fetch(a.request);
+      return n.status === 200 && c.put(a.request, n.clone()), n;
     } catch {
-      return n.match(a.request);
+      return c.match(a.request);
     }
   };
   a.respondWith(t());
