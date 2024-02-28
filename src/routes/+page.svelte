4f5347eeb859dff9ef90550557addffc0200e008
Janessa Huppi <jhuppicode@gmail.com>
2024-02-28 10:34:48 -0700

    Minor UI tweak for soom header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <h1>soom</h1>
 
 <style>
-    h1 {
-        font-size: var(--scale-10);
-        line-height: 1;
-        margin: 0 auto 0;
-        text-align: center;
-    }
+	h1 {
+		font-size: var(--scale-fluid-10);
+		line-height: 1;
+		margin: 0 auto 0;
+		text-align: center;
+	}
 </style>
